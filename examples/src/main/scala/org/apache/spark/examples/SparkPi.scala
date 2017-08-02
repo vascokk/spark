87743be064855f4d3d242266403b0602a5958526
ArtRand <arand@soe.ucsc.edu>
2017-08-02 16:36:38 -0700

    reverted sparkPi back

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -29,9 +29,6 @@ object SparkPi {
       .builder
       .appName("Spark Pi")
       .getOrCreate()
-    sys.env.foreach {
-      case (key, value) => println(s"key: $key value $value")
-    }
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
@@ -39,9 +36,7 @@ object SparkPi {
       val y = random * 2 - 1
       if (x*x + y*y <= 1) 1 else 0
     }.reduce(_ + _)
-    println("I used $slices to determine that Pi is roughly " + 4.0 * count / (n - 1))
-    val contents = scala.io.Source.fromFile(args(1)).mkString
-    println(s"Contents of file ${args(1)}: ${contents}")
+    println("Pi is roughly " + 4.0 * count / (n - 1))
     spark.stop()
   }
 }
