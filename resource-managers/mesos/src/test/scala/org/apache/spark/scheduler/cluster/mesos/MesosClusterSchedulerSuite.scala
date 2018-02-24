4812ba3d10264f6d22ec654fa16b5810d70c27a9
Susan X. Huynh <xhuynh@mesosphere.com>
2018-02-24 13:07:49 -0800

    [MESOS][SPARK-23464] Fix Mesos Cluster Scheduler to allow configs containing spaces and quotes

--- resource-managers/mesos/src/test/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterSchedulerSuite.scala
+++ resource-managers/mesos/src/test/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterSchedulerSuite.scala
@@ -199,6 +199,31 @@ class MesosClusterSchedulerSuite extends SparkFunSuite with LocalSparkContext wi
     })
   }
 
+  test("can pass config with spaces and quotes") {
+    setScheduler()
+
+    val mem = 1000
+    val cpu = 1
+
+    val response = scheduler.submitDriver(
+      new MesosDriverDescription("d1", "jar", mem, cpu, true,
+        command,
+        Map("spark.mesos.executor.home" -> "test",
+          "spark.app.name" -> "test",
+          "spark.driver.extraJavaOptions" ->
+            "-XX:+PrintGC -Dkey=value -Dnum=\"one two\""),
+        "s1",
+        new Date()))
+    assert(response.success)
+
+    val offer = Utils.createOffer("o1", "s1", mem, cpu)
+    scheduler.resourceOffers(driver, List(offer).asJava)
+    val tasks = Utils.verifyTaskLaunched(driver, "o1")
+    val driverCmd = tasks.head.getCommand.getValue
+    assert(driverCmd.contains(
+      "--conf spark.driver.extraJavaOptions=\"-XX:+PrintGC -Dkey=value -Dnum=\\\"one two\\\"\" "))
+  }
+
   test("supports spark.mesos.driverEnv.*") {
     setScheduler()
 
